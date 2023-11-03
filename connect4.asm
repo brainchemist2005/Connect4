1683e17188030e9ac96350653f62a60f04ca0bad
brainchemist2005 <zackariaebouargan@gmail.com>
2023-11-03 04:16:11 -0400

    I added some routines, loaded the strings that I will be using. Put some todos througout the code

--- connect4.asm
+++ connect4.asm
@@ -4,6 +4,8 @@
 .eqv Exit, 10
 
 .data
+Board: .word L1 L2 L3 L4 L5 L6
+
 L1 : .string  ":.......:"
 L2 : .string  ":.......:"
 L3 : .string  ":.......:"
@@ -13,26 +15,77 @@ L6 : .string  ":.......:"
 
 .text
 li t0,'\n'
-la s1,L1
-la s2,L2
-la s3,L3
-la s4,L4
-la s5,L5
-la s6,L6
+la s0,Board
+lw s1, (s0)
+lw s2, 4(s0)
+lw s3, 8(s0)
+lw s4, 12(s0)
+lw s5, 16(s0)
+lw s6, 20(s0)
 
 li t1,'X'
 li t2,'O'
 li t3,'d'
 
 inGame:
+	li t6 , 1 #this counter keeps track of the turn of the players if the counter is a pair number then 
+	 	  #the X will play otherwise it's for the O to play
 	li a7, ReadChar
 	ecall
 	
+	beq a0,'q' , Exit
 	beq a0 , t3, gameStatus
-	beq a0 , 1, gameAction
+	bgt a0 , 0, gameActionCheck
+	
+	
+	addi t6 ,t6,1
+	
+	
+gameActionCheck:
+	blt a0,8,gameAction
+	j Exit  #wrong input 
 	
 gameAction:
-	beq a0 , 1
+	
+			
+copy_loop:
+    lb t5, 0(s1)   # Load a character from originalString
+    beq t4,9, copy_done  # If null terminator is reached, exit the loop
+
+    # Check if this is the character at position 3
+    li t4, a0 # The position to change
+    beq t0, t3, change_character  # If it's the character at position 3, go to change_character
+
+    # Copy the character to modifiedString
+    sb t2, 0(a1)
+
+    # Move to the next character
+    addi a0, a0, 1
+    addi a1, a1, 1
+    addi t4, t4, 1
+    j copy_loop
+	
+change_character:
+    # Change the character at position 3 (0-based index)
+    li t2, 'X'  # Change it to 'X'
+
+    # Copy the modified character to modifiedString
+    sb t2, 0(a1)
+
+    # Move to the next character
+    addi a0, a0, 1
+    addi a1, a1, 1
+    addi t0, t0, 1
+    j copy_loop
+    
+    
+copy_done:
+    # Add a null terminator to the modifiedString
+    li t2, 0
+    sb t2, 0(a1)
+	
+isOdd:
+	#check the counter to see if which player is playing
 
 gameStatus:
 	li a7, PrintChar
