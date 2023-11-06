1628ae4d295c9f4ef3d9e694487b417043e2e725
brainchemist2005 <zackariaebouargan@gmail.com>
2023-11-06 00:16:55 -0500

    All lot of errors will debug tomorrow

--- connect4.asm
+++ connect4.asm
@@ -4,132 +4,139 @@
 .eqv Exit, 10
 
 .data
-Board: .word L1 L2 L3 L4 L5 L6
+Board: .word L1 L2 L3 L4 L5 L6 L7
 
-L1 : .string  ":.......:"
-L2 : .string  ":.......:"
-L3 : .string  ":.......:"
-L4 : .string  ":.......:"
-L5 : .string  ":.......:"
-L6 : .string  ":.......:"
+L1: .string ":.......:"
+L2: .string ":.......:"
+L3: .string ":.......:"
+L4: .string ":.......:"
+L5: .string ":.......:"
+L6: .string ":.......:"
+L7: .string ":.......:"
+L8: .space 13
 
 .text
-li t0,'\n'
-la s0,Board
-lw s1, (s0)
-lw s2, 4(s0)
-lw s3, 8(s0)
-lw s4, 12(s0)
-lw s5, 16(s0)
-lw s6, 20(s0)
+li t0, '\n'
+la a0, Board
+lw a1, (a0)
+lw a2, 4(a0)
+lw a3, 8(a0)
+lw a4, 12(a0)
+lw a5, 16(a0)
+lw a6, 20(a0)
+lw a7, 24(a0)
 
-li t1,'X'
-li t2,'O'
-li t3,'d'
+li t1, 'X'
+li t2, 'O'
 
-inGame:
-	li t6 , 1 #this counter keeps track of the turn of the players if the counter is a pair number then 
-	 	  #the X will play otherwise it's for the O to play
-	li a7, ReadChar
-	ecall
-	
-	beq a0,'q' , Exit
-	beq a0 , t3, gameStatus
-	bgt a0 , 0, gameActionCheck
-	
-	
-	addi t6 ,t6,1
-	
-	
+
+main:
+    li t3, 'd'
+    li t6, 1  # Counter to keep track of player's turn
+    
+    li a7, ReadChar
+    ecall
+    
+    beq a0, t3, gameStatus
+    li t3, 'q'
+    beq a0, t3, Done
+    bgtz a0,gameActionCheck
+    j Done
+
 gameActionCheck:
-	blt a0,8,gameAction
-	j Exit  #wrong input 
-	
+    li t3 , 8
+    bgt a0, t3, Done
+    jal gameAction
+
 gameAction:
-	
-			
-copy_loop:
-    lb t5, 0(s1)   # Load a character from originalString
-    beq t4,9, copy_done  # If null terminator is reached, exit the loop
-
-    # Check if this is the character at position 3
-    li t4, a0 # The position to change
-    beq t0, t3, change_character  # If it's the character at position 3, go to change_character
-
-    # Copy the character to modifiedString
-    sb t2, 0(a1)
-
-    # Move to the next character
-    addi a0, a0, 1
-    addi a1, a1, 1
-    addi t4, t4, 1
-    j copy_loop
-	
-change_character:
-    # Change the character at position 3 (0-based index)
-    li t2, 'X'  # Change it to 'X'
-
-    # Copy the modified character to modifiedString
-    sb t2, 0(a1)
-
-    # Move to the next character
-    addi a0, a0, 1
-    addi a1, a1, 1
-    addi t0, t0, 1
-    j copy_loop
+    li t3, '.'
+    addi t6, t6, 1
     
+    # Set up a pointer to the first row
+    add t8, a1, x0
+
+
+    # Loop through the rows
+    rowLoop:
+        beqz t8, done  # Exit the loop if the pointer is null (end of rows)
+
+        # Check the 3rd slot in the current row for '.'
+        lb t9, 2(t8)
+        beq t9, t3, slotFound  # If the 3rd slot contains '.', jump to slotFound
+
+        # Move to the next row
+        lw t8, 4(t8)
+        j rowLoop
+
+    done:
+    j main
     
-copy_done:
-    # Add a null terminator to the modifiedString
-    li t2, 0
-    sb t2, 0(a1)
-	
-isOdd:
-	#check the counter to see if which player is playing
+slotFound: 
+    slotFound:
+    # Determine the symbol ('X' or 'O') based on the value of t6 (player's turn)
+    li t4, 'X'
+    li t5, 'O'
+    
+    # Check if t6 is even or odd
+    rem t7, t6, 2  # t7 will be 0 if t6 is even, 1 if t6 is odd
+
+    # Calculate the symbol to use
+    beqz t7, setX  # If t7 is 0 (even), jump to setX
+    setO:
+        mv t4, t5  # If t6 is odd, set t4 to 'O'
+        j updateSlot
+
+    setX:
+        # If t6 is even, t4 is already 'X'
+
+    updateSlot:
+        # Update the 3rd character of the current row with the chosen symbol (t4)
+        sb t4, 2(a1)
+        
+    j main
 
 gameStatus:
 	li a7, PrintChar
 	mv a0, t0
 	ecall
 	li a7, PrintString
-	mv a0, s1
+	mv a0, a1
 	ecall
 	li a7, PrintChar
 	mv a0, t0
 	ecall
 	li a7, PrintString	
-	mv a0, s2
+	mv a0, a2
 	ecall
 	li a7, PrintChar	
 	mv a0,t0
 	ecall
 	li a7, PrintString	
-	mv a0, s3
+	mv a0, a3
 	ecall
 	li a7, PrintChar	
 	mv a0,t0
 	ecall
 	li a7, PrintString	
-	mv a0, s4
+	mv a0, a4
 	ecall
 	li a7, PrintChar	
 	mv a0,t0
 	ecall
 	li a7, PrintString	
-	mv a0, s5
+	mv a0, a5
 	ecall
 	li a7, PrintChar	
 	mv a0,t0
 	ecall
 	li a7, PrintString	
-	mv a0, s6
+	mv a0, a6
 	ecall
 	li a7, PrintChar	
 	mv a0,t0
 	ecall
 	j inGame
 	
-	
-Exit:
+Done:
 	li a7,Exit
 	ecall
