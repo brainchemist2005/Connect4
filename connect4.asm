4db23d77595c770f00d3d652f3151bf9b67d8e4e
brainchemist2005 <zackariaebouargan@gmail.com>
2023-11-06 20:00:44 -0500

    I advanced in the code

--- connect4.asm
+++ connect4.asm
@@ -16,23 +16,18 @@ L7: .string ":.......:"
 L8: .space 13
 
 .text
-li t0, '\n'
-la a0, Board
-lw a1, (a0)
-lw a2, 4(a0)
-lw a3, 8(a0)
-lw a4, 12(a0)
-lw a5, 16(a0)
-lw a6, 20(a0)
-lw a7, 24(a0)
-
-li t1, 'X'
-li t2, 'O'
-
+la s0, Board
+lw s1, (s0)
+lw s2, 4(s0)
+lw s3, 8(s0)
+lw s4, 12(s0)
+lw s5, 16(s0)
+lw s6, 20(s0)
+lw s7, 24(s0)
 
 main:
     li t3, 'd'
-    li t6, 1  # Counter to keep track of player's turn
+    li t6, 1  # Counter to keep track of the player's turn
     
     li a7, ReadChar
     ecall
@@ -40,48 +35,46 @@ main:
     beq a0, t3, gameStatus
     li t3, 'q'
     beq a0, t3, Done
-    bgtz a0,gameActionCheck
+    bgtz a0, gameActionCheck
     j Done
 
 gameActionCheck:
-    li t3 , 8
+    li t3, 56
     bgt a0, t3, Done
     jal gameAction
 
 gameAction:
     li t3, '.'
-    addi t6, t6, 1
-    
-    # Set up a pointer to the first row
-    add t8, a1, x0
 
+    # Set up a pointer to the first row
+    add t0, s0, x0
 
     # Loop through the rows
     rowLoop:
-        beqz t8, done  # Exit the loop if the pointer is null (end of rows)
+        beqz t0, done  # Exit the loop if the pointer is null (end of rows)
 
         # Check the 3rd slot in the current row for '.'
-        lb t9, 2(t8)
-        beq t9, t3, slotFound  # If the 3rd slot contains '.', jump to slotFound
+        lb t1, 2(t0)
+        beq t1, t3, slotFound  # If the 3rd slot contains '.', jump to slotFound
 
         # Move to the next row
-        lw t8, 4(t8)
+        lw t0, 4(t0)
         j rowLoop
 
     done:
     j main
-    
+
 slotFound: 
-    slotFound:
     # Determine the symbol ('X' or 'O') based on the value of t6 (player's turn)
     li t4, 'X'
     li t5, 'O'
-    
+    li t2, 2  # Store the value 2 in t2
+
     # Check if t6 is even or odd
-    rem t7, t6, 2  # t7 will be 0 if t6 is even, 1 if t6 is odd
+    rem t2, t6, t2  # t2 will be 0 if t6 is even, 1 if t6 is odd
 
     # Calculate the symbol to use
-    beqz t7, setX  # If t7 is 0 (even), jump to setX
+    beqz t2, setX  # If t2 is 0 (even), jump to setX
     setO:
         mv t4, t5  # If t6 is odd, set t4 to 'O'
         j updateSlot
@@ -89,54 +82,58 @@ slotFound:
     setX:
         # If t6 is even, t4 is already 'X'
 
-    updateSlot:
-        # Update the 3rd character of the current row with the chosen symbol (t4)
-        sb t4, 2(a1)
-        
+updateSlot:
+    # Update the 3rd character of the current row with the chosen symbol (t4)
+    sb t4, 2(t0)
+    addi t6, t6, 1  # Increment the player's turn counter
+    li a0, 999
+    li a7, 1
+    ecall
     j main
 
 gameStatus:
-	li a7, PrintChar
-	mv a0, t0
-	ecall
-	li a7, PrintString
-	mv a0, a1
-	ecall
-	li a7, PrintChar
-	mv a0, t0
-	ecall
-	li a7, PrintString	
-	mv a0, a2
-	ecall
-	li a7, PrintChar	
-	mv a0,t0
-	ecall
-	li a7, PrintString	
-	mv a0, a3
-	ecall
-	li a7, PrintChar	
-	mv a0,t0
-	ecall
-	li a7, PrintString	
-	mv a0, a4
-	ecall
-	li a7, PrintChar	
-	mv a0,t0
-	ecall
-	li a7, PrintString	
-	mv a0, a5
-	ecall
-	li a7, PrintChar	
-	mv a0,t0
-	ecall
-	li a7, PrintString	
-	mv a0, a6
-	ecall
-	li a7, PrintChar	
-	mv a0,t0
-	ecall
-	j inGame
-	
+    li t0, '\n'
+    li a7, PrintChar
+    mv a0, t0
+    ecall
+    li a7, PrintString
+    mv a0, s1
+    ecall
+    li a7, PrintChar
+    mv a0, t0
+    ecall
+    li a7, PrintString
+    mv a0, s2
+    ecall
+    li a7, PrintChar
+    mv a0, t0
+    ecall
+    li a7, PrintString
+    mv a0, s3
+    ecall
+    li a7, PrintChar
+    mv a0, t0
+    ecall
+    li a7, PrintString
+    mv a0, s4
+    ecall
+    li a7, PrintChar
+    mv a0, t0
+    ecall
+    li a7, PrintString
+    mv a0, s5
+    ecall
+    li a7, PrintChar
+    mv a0, t0
+    ecall
+    li a7, PrintString
+    mv a0, s6
+    ecall
+    li a7, PrintChar
+    mv a0, t0
+    ecall
+    j main
+
 Done:
-	li a7,Exit
-	ecall
+    li a7, Exit
+    ecall
